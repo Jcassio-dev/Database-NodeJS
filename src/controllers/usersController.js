41927bddbb64d406bf7bd1db2af15d47a1a5d105
Jose Cassio <josecassio2013@gmail.com>
2022-10-23 21:14:00 -0300

    Add Knex

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,6 @@
 const AppError = require('../utils/appError')
 
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 
 const sqliteConnection = require('../database/sqlite')
 
@@ -33,14 +33,13 @@ class usersController {
 
     }
     async update(request, response) {
-        const { name, email } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
 
 
         const database = await sqliteConnection();
         const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
         
-        console.log(user, name, email, id)
 
         if(!user) {
             throw new AppError("Usuário não encontrado!")
@@ -48,22 +47,39 @@ class usersController {
         
 
         const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
-        
+
         if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
             throw new AppError("Este e-mail já está em uso!");
         }
 
 
-        user.name = name;
-        user.email = email;
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+
+        if(password && !old_password){
+            throw new AppError("Você precisa informar a senha antiga para atualizar a senha")
+        }
+
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password)
+
+            if(!checkOldPassword){
+                throw new AppError("A senha antiga não confere.");
+            }
+
+            user.password = await hash(password, 8)
+        }
+
 
         await database.run(`
             UPDATE users SET
             name = ?,
             email = ?,
-            update_at = ?
+            password = ?,
+            updated_at = DATETIME('now')
             WHERE id = ?`,
-            [user.name, user.email, new Date(), id]
+            [user.name, user.email, user.password, id]
             );
 
         return response.json();
